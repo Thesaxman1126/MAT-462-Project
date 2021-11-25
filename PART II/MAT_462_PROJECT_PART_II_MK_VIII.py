c27d37c9d9b5edaa5c5e1e79e66328f3a635eee8
Thesaxman1126 <87177857+Thesaxman1126@users.noreply.github.com>
2021-11-24 22:05:23 -0700

    Update MAT_462_PROJECT_PART_II_MK_VIII.py

--- PART II/MAT_462_PROJECT_PART_II_MK_VIII.py
+++ PART II/MAT_462_PROJECT_PART_II_MK_VIII.py
@@ -5,7 +5,7 @@ Created on Tue Oct 26 16:55:04 2021
 @author: Nick Navas
 @class: MAT 462
 
-Used to generate figures 4-8    
+Used to generate figures 7-12    
 """
 # IMPORTS #
 import scipy.special #Used for Bessel I 
