99c05e3b8d79c8e6381a2c87dabedd6930147643
Nick <87177857+Thesaxman1126@users.noreply.github.com>
2021-12-05 18:07:43 -0700

    Update and rename l2_norm.m to l2_norm_v2.m

--- PART IV/MATLAB_REWRITE/l2_norm_v2.m
+++ PART IV/MATLAB_REWRITE/l2_norm_v2.m
@@ -1,5 +1,5 @@
 %% MAT 462 PART IV: L2 NORM 
 
-function l2 = l2_norm(array,nr,nz)
+function l2 = l2_norm_v2(array,nr,nz)
     l2 = sqrt(1/((nr+1)*(nz+1)) * sum(array.^2, 'all')); % Definition of discrete l2 norm
 end
